5e0d640a94648fab9170eb80d2cea9cc1aa9f5e9
Khushi Khari <144012000+khushikhari@users.noreply.github.com>
2024-08-29 20:47:44 +0530

    Update RegisterPage.js

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,14 +1,15 @@
-import {useState} from "react";
+import { useState } from "react";
 
 export default function RegisterPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
   async function register(ev) {
     ev.preventDefault();
-    const response = await fetch('http://localhost:4000/register', {
+    const response = await fetch('https://marvelblog-mi9u.vercel.app/register', {
       method: 'POST',
-      body: JSON.stringify({username,password}),
-      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
     });
     if (response.status === 200) {
       alert('registration successful');
@@ -16,17 +17,22 @@ export default function RegisterPage() {
       alert('registration failed');
     }
   }
+
   return (
     <form className="register" onSubmit={register}>
       <h1>Register</h1>
-      <input type="text"
-             placeholder="username"
-             value={username}
-             onChange={ev => setUsername(ev.target.value)}/>
-      <input type="password"
-             placeholder="password"
-             value={password}
-             onChange={ev => setPassword(ev.target.value)}/>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={ev => setUsername(ev.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={ev => setPassword(ev.target.value)}
+      />
       <button>Register</button>
     </form>
   );
