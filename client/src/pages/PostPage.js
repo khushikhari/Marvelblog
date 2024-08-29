95937a3aebf3d7a4108557dd3afc429bc0aa21be
Khushi Khari <144012000+khushikhari@users.noreply.github.com>
2024-08-29 20:52:58 +0530

    Update PostPage.js

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,21 +1,21 @@
-import {useContext, useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
-import {formatISO9075} from "date-fns";
-import {UserContext} from "../UserContext";
-import {Link} from 'react-router-dom';
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { formatISO9075 } from "date-fns";
+import { UserContext } from "../UserContext";
+import { Link } from "react-router-dom";
 
 export default function PostPage() {
-  const [postInfo,setPostInfo] = useState(null);
-  const {userInfo} = useContext(UserContext);
-  const {id} = useParams();
+  const [postInfo, setPostInfo] = useState(null);
+  const { userInfo } = useContext(UserContext);
+  const { id } = useParams();
+
   useEffect(() => {
-    fetch(`http://localhost:4000/post/${id}`)
-      .then(response => {
-        response.json().then(postInfo => {
-          setPostInfo(postInfo);
-        });
+    fetch(`https://marvelblog-mi9u.vercel.app/post/${id}`)
+      .then(response => response.json())
+      .then(postInfo => {
+        setPostInfo(postInfo);
       });
-  }, []);
+  }, [id]);
 
   if (!postInfo) return '';
 
@@ -35,9 +35,9 @@ export default function PostPage() {
         </div>
       )}
       <div className="image">
-        <img src={`http://localhost:4000/${postInfo.cover}`} alt=""/>
+        <img src={`https://marvelblog-mi9u.vercel.app/${postInfo.cover}`} alt="" />
       </div>
-      <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}} />
+      <div className="content" dangerouslySetInnerHTML={{ __html: postInfo.content }} />
     </div>
   );
 }
