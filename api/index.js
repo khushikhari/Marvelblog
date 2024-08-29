a65d9946b951b8e5fac882d04beb4d3358f563b1
Khushi Khari <144012000+khushikhari@users.noreply.github.com>
2024-08-29 20:37:53 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,12 +14,24 @@ const fs = require('fs');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+const allowedOrigins = ['http://localhost:3000', 'https://your-app.vercel.app'];
+
+app.use(cors({
+  credentials: true,
+  origin: function(origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-mongoose.connect('')
+mongoose.connect('mongodb+srv://khushikhari03:pV10XiFLuz30JA9S@cluster0.xwy3k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 app.post('/register', async (req,res) => {
   const {username,password} = req.body;
   try{
