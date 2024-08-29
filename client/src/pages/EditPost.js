debff696aa14b1adda09b67a984094abff3da1e6
Khushi Khari <144012000+khushikhari@users.noreply.github.com>
2024-08-29 20:49:10 +0530

    Update EditPost.js

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -1,17 +1,17 @@
-import {useEffect, useState} from "react";
-import {Navigate, useParams} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Navigate, useParams } from "react-router-dom";
 import Editor from "../Editor";
 
 export default function EditPost() {
-  const {id} = useParams();
-  const [title,setTitle] = useState('');
-  const [summary,setSummary] = useState('');
-  const [content,setContent] = useState('');
+  const { id } = useParams();
+  const [title, setTitle] = useState('');
+  const [summary, setSummary] = useState('');
+  const [content, setContent] = useState('');
   const [files, setFiles] = useState('');
-  const [redirect,setRedirect] = useState(false);
+  const [redirect, setRedirect] = useState(false);
 
   useEffect(() => {
-    fetch('http://localhost:4000/post/'+id)
+    fetch(`https://marvelblog-mi9u.vercel.app/post/${id}`)
       .then(response => {
         response.json().then(postInfo => {
           setTitle(postInfo.title);
@@ -19,7 +19,7 @@ export default function EditPost() {
           setSummary(postInfo.summary);
         });
       });
-  }, []);
+  }, [id]);
 
   async function updatePost(ev) {
     ev.preventDefault();
@@ -31,7 +31,7 @@ export default function EditPost() {
     if (files?.[0]) {
       data.set('file', files?.[0]);
     }
-    const response = await fetch('http://localhost:4000/post', {
+    const response = await fetch('https://marvelblog-mi9u.vercel.app/post', {
       method: 'PUT',
       body: data,
       credentials: 'include',
@@ -42,23 +42,29 @@ export default function EditPost() {
   }
 
   if (redirect) {
-    return <Navigate to={'/post/'+id} />
+    return <Navigate to={`/post/${id}`} />;
   }
 
   return (
     <form onSubmit={updatePost}>
-      <input type="title"
-             placeholder={'Title'}
-             value={title}
-             onChange={ev => setTitle(ev.target.value)} />
-      <input type="summary"
-             placeholder={'Summary'}
-             value={summary}
-             onChange={ev => setSummary(ev.target.value)} />
-      <input type="file"
-             onChange={ev => setFiles(ev.target.files)} />
+      <input 
+        type="title"
+        placeholder="Title"
+        value={title}
+        onChange={ev => setTitle(ev.target.value)} 
+      />
+      <input 
+        type="summary"
+        placeholder="Summary"
+        value={summary}
+        onChange={ev => setSummary(ev.target.value)} 
+      />
+      <input 
+        type="file"
+        onChange={ev => setFiles(ev.target.files)} 
+      />
       <Editor onChange={setContent} value={content} />
-      <button style={{marginTop:'5px'}}>Update post</button>
+      <button style={{marginTop: '5px'}}>Update post</button>
     </form>
   );
 }
