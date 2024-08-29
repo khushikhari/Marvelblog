eb41b791d19b2407242ea8d06a8e13f1a6725980
Khushi Khari <144012000+khushikhari@users.noreply.github.com>
2024-08-29 20:50:26 +0530

    Update IndexPage.js

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -1,19 +1,21 @@
 import Post from "../Post";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export default function IndexPage() {
-  const [posts,setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+
   useEffect(() => {
-    fetch('http://localhost:4000/post').then(response => {
-      response.json().then(posts => {
+    fetch('https://marvelblog-mi9u.vercel.app/post')
+      .then(response => response.json())
+      .then(posts => {
         setPosts(posts);
       });
-    });
   }, []);
+
   return (
     <>
       {posts.length > 0 && posts.map(post => (
-        <Post {...post} />
+        <Post key={post._id} {...post} />
       ))}
     </>
   );
